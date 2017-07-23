a93c14de4a37c5434f7f136a3b845409823ad12d
David Cady Jr <davidcadyjr@mac.com>
2017-07-23 11:11:39 -0500

    get, post, patch, delete working on backend

--- server/controllers/AlbumController.rb
+++ server/controllers/AlbumController.rb
@@ -1,3 +1,50 @@
 class AlbumController < ApplicationController
 
+	#get request to /albums
+	get '/' do 
+		albums = Album.all
+		albums.to_json
+	end
+
+	#get request to /albums/id to get specific album info
+	get '/:id' do
+		id = params[:id]
+		album = Album.find(id)
+		{album: album}.to_json
+	end
+
+	#post request to /albums. saves a new album to the users account
+	post '/' do
+		new_album = JSON.parse(request.body.read)
+		album = Album.new(new_album)
+		album.save
+		album.to_json
+
+		"success"
+	end
+
+	patch '/:id' do
+
+		id = params[:id]
+		album = Album.find(id)
+		updated_album = JSON.parse(request.body.read)
+		album.update_attributes(updated_album)
+		album.save
+		album.to_json
+
+		"success"
+	end
+
+	delete '/:id' do
+
+		id = params[:id]
+		album = Album.find(id)
+		album.destroy
+		albums = Album.all
+		albums.to_json
+
+		"success"
+	end
+
+
 end
